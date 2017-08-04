c3934d808f452071b29890789be0a8cd6f32e7c4
neka-nat <nekanat.stock@gmail.com>
2017-08-05 08:26:06 +0900

    tensorboard-chainer

--- demo.py
+++ demo.py
@@ -1,26 +1,50 @@
-import torch
-import torchvision.utils as vutils
+import math
+import chainer
+import cupy
 import numpy as np
-import torchvision.models as models
 from datetime import datetime
 from tensorboard import SummaryWriter
-resnet18 = models.resnet18(True)
+
+def make_grid(tensor, padding=2):
+    """
+    Given a 4D mini-batch Tensor of shape (B x C x H x W), makes a grid of images
+    """
+    # make the mini-batch of images into a grid
+    nmaps = tensor.shape[0]
+    xmaps = int(nmaps**0.5)
+    ymaps = int(math.ceil(nmaps / xmaps))
+    height, width = int(tensor.shape[2] + padding), int(tensor.shape[3] + padding)
+    grid = np.ones((3, height*ymaps, width*xmaps))
+    k = 0
+    sy = 1 + padding // 2
+    for y in range(ymaps): 
+        sx = 1 + padding // 2
+        for x in range(xmaps):
+            if k >= nmaps:
+                break
+            grid[:, sy:sy+height-padding, sx:sx+width-padding] = tensor[k]
+            sx += width
+            k = k + 1
+        sy += height
+    return grid
+
+vgg = chainer.links.VGG16Layers()
 writer = SummaryWriter('runs/'+datetime.now().strftime('%B%d  %H:%M:%S'))
 sample_rate = 44100
 freqs = [262, 294, 330, 349, 392, 440, 440, 440, 440, 440, 440]
 for n_iter in range(100):
-    M_global = torch.rand(1) # value to keep
+    M_global = cupy.random.rand(1) # value to keep
     writer.add_scalar('M_global', M_global[0], n_iter)
-    x = torch.rand(32, 3, 64, 64) # output from network
+    x = np.random.rand(32, 3, 64, 64) # output from network
     if n_iter%10==0:
-        x = vutils.make_grid(x, normalize=True, scale_each=True)   
+        x = make_grid(x)
         writer.add_image('Image', x, n_iter)
-        x = torch.zeros(sample_rate*2)
-        for i in range(x.size(0)):
+        x = np.zeros(sample_rate*2)
+        for i in range(x.shape[0]):
             x[i] = np.cos(freqs[n_iter//10]*np.pi*float(i)/float(sample_rate)) # sound amplitude should in [-1, 1]
         writer.add_audio('Audio', x, n_iter)
-        for name, param in resnet18.named_parameters():
-            writer.add_histogram(name, param.clone().cpu().data.numpy(), n_iter)
+        for name, param in vgg.namedparams():
+            writer.add_histogram(name, chainer.cuda.to_cpu(param.data), n_iter)
         writer.add_text('Text', 'text logged at step:'+str(n_iter), n_iter)
         writer.add_text('another Text', 'another text logged at step:'+str(n_iter), n_iter)
         
--- tensorboard/embedding.py
+++ tensorboard/embedding.py
@@ -1,4 +1,5 @@
-import torch
+import cupy
+import utils
 import os
 
 def make_tsv(metadata, save_path):
@@ -11,15 +12,14 @@ def make_tsv(metadata, save_path):
 # https://github.com/tensorflow/tensorboard/issues/44 image label will be squared
 def make_sprite(label_img, save_path):
     import math
-    import torchvision
-    nrow = int(math.floor(math.sqrt(label_img.size(0))))
-    xx = torchvision.utils.make_grid(torch.Tensor(1,3,32,32), padding=0)
-    if xx.size(2)==33: # https://github.com/pytorch/vision/issues/206
-        sprite = torchvision.utils.make_grid(label_img, nrow=nrow, padding=0)
+    nrow = int(math.floor(math.sqrt(label_img.shape(0))))
+    xx = utils.make_grid(cupy.zeros((1,3,32,32)), padding=0)
+    if xx.shape(2)==33: # https://github.com/pytorch/vision/issues/206
+        sprite = utils.make_grid(label_img, nrow=nrow, padding=0)
         sprite = sprite[:,1:,1:]
-        torchvision.utils.save_image(sprite, os.path.join(save_path, 'sprite.png'))
+        utils.save_image(sprite, os.path.join(save_path, 'sprite.png'))
     else:
-        torchvision.utils.save_image(label_img, os.path.join(save_path, 'sprite.png'), nrow=nrow, padding=0)
+        utils.save_image(label_img, os.path.join(save_path, 'sprite.png'), nrow=nrow, padding=0)
 
 def make_pbtxt(save_path, metadata, label_img):
     with open(os.path.join(save_path, 'projector_config.pbtxt'), 'w') as f:
@@ -30,8 +30,8 @@ def make_pbtxt(save_path, metadata, label_img):
         if label_img is not None:
             f.write('sprite {\n')
             f.write('image_path: "sprite.png"\n')
-            f.write('single_image_dim: {}\n'.format(label_img.size(3)))
-            f.write('single_image_dim: {}\n'.format(label_img.size(2)))
+            f.write('single_image_dim: {}\n'.format(label_img.shape(3)))
+            f.write('single_image_dim: {}\n'.format(label_img.shape(2)))
             f.write('}\n')
         f.write('}\n')
 
@@ -50,10 +50,10 @@ def add_embedding(mat, save_path, metadata=None, label_img=None):
     except OSError:
         print('warning: dir exists')
     if metadata is not None:
-        assert mat.size(0)==len(metadata), '#labels should equal with #data points'
+        assert mat.shape(0)==len(metadata), '#labels should equal with #data points'
         make_tsv(metadata, save_path)
     if label_img is not None:
-        assert mat.size(0)==label_img.size(0), '#images should equal with #data points'
+        assert mat.shape(0)==label_img.shape(0), '#images should equal with #data points'
         make_sprite(label_img, save_path)
     import tensorflow as tf
     tf.reset_default_graph()
