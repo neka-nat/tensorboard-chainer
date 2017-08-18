00b4ed0d9d4d48f646c4e94ef9c4b077879bbabe
neka-nat <nekanat.stock@gmail.com>
2017-08-18 23:58:39 +0900

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setup(
     name='tensorboard-chainer',
-    version='0.1',
+    version='0.1.1',
     description='Log TensorBoard events with chainer',
     author='nake nat',
     author_email='nakanat.stock@gmail.com',
