eb4f6f7981db6cce8ebd23fd78d21381e83fb332
neka-nat <nekanat.stock@gmail.com>
2017-09-04 23:46:09 +0900

    fix setting name scope

--- tensorboard/name_scope.py
+++ tensorboard/name_scope.py
@@ -1,7 +1,12 @@
+import chainer
 from chainer import function
 from chainer import variable
 import functools
-from types import MethodType
+from types import FunctionType, MethodType
+
+def copy_method(c):
+    g = MethodType(c.__init__, None, c)
+    return g
 
 def _init_with_name_scope(self, *args, **kargs):
     self.name_scope = kargs['name_scope']
@@ -10,8 +15,36 @@ def _init_with_name_scope(self, *args, **kargs):
     del kargs['org_init']
     org_init(self, *args, **kargs)
 
-_org_func_init = function.Function.__init__
-_org_val_init = variable.VariableNode.__init__
+_org_classes = [function.Function,
+                chainer.functions.activation.clipped_relu.ClippedReLU,
+                chainer.functions.activation.crelu.CReLU,
+                chainer.functions.activation.elu.ELU,
+                chainer.functions.activation.leaky_relu.LeakyReLU,
+                chainer.functions.array.concat.Concat,
+                chainer.functions.array.reshape.Reshape,
+                chainer.functions.math.basic_math.AddConstant,
+                chainer.functions.math.basic_math.SubFromConstant,
+                chainer.functions.math.basic_math.MulConstant,
+                chainer.functions.math.basic_math.DivFromConstant,
+                chainer.functions.math.basic_math.PowVarConst,
+                chainer.functions.math.basic_math.PowConstVar,
+                chainer.functions.math.basic_math.MatMulVarConst,
+                chainer.functions.math.basic_math.MatMulConstVar,
+                chainer.functions.math.sum.Sum,
+                chainer.functions.connection.convolution_2d.Convolution2DFunction,
+                chainer.functions.connection.convolution_nd.ConvolutionND,
+                chainer.functions.connection.deconvolution_2d.Deconvolution2DFunction,
+                chainer.functions.connection.deconvolution_nd.DeconvolutionND,
+                chainer.functions.pooling.average_pooling_2d.AveragePooling2D,
+                chainer.functions.pooling.average_pooling_nd.AveragePoolingND,
+                chainer.functions.pooling.max_pooling_2d.MaxPooling2D,
+                chainer.functions.pooling.max_pooling_nd.MaxPoolingND,
+                chainer.functions.pooling.pooling_2d.Pooling2D,
+                chainer.functions.pooling.pooling_nd._PoolingND,
+                chainer.functions.pooling.unpooling_2d.Unpooling2D,
+                chainer.functions.pooling.unpooling_nd.UnpoolingND,
+                variable.VariableNode]
+_copy_org_inits = [copy_method(c) for c in _org_classes]
 
 class name_scope(object):
     """Class that creates hierarchical names for operations and variables.
@@ -30,25 +63,22 @@ class name_scope(object):
     stack = []
     def __init__(self, name, values=[]):
         self.stack.append(name)
+        self._org_inits = []
         for v in values:
             v.node.name_scope = '/'.join(self.stack)
 
     def __enter__(self):
-        self._org_func_init = function.Function.__init__
-        function.Function.__init__ = MethodType(functools.partial(_init_with_name_scope,
-                                                                  name_scope='/'.join(self.stack),
-                                                                  org_init=_org_func_init),
-                                                None, function.Function)
-        self._org_val_init = variable.VariableNode.__init__
-        variable.VariableNode.__init__ = MethodType(functools.partial(_init_with_name_scope,
-                                                                      name_scope='/'.join(self.stack),
-                                                                      org_init=_org_val_init),
-                                                    None, variable.VariableNode)
+        for idx, c in enumerate(_org_classes):
+            self._org_inits.append(c.__init__)
+            c.__init__ = MethodType(functools.partial(_init_with_name_scope,
+                                                      name_scope='/'.join(self.stack),
+                                                      org_init=_copy_org_inits[idx]),
+                                    None, c)
         return self
 
     def __exit__(self, exec_type, exec_value, traceback):
-        function.Function.__init__ = self._org_func_init
-        variable.VariableNode.__init__ = self._org_val_init
+        for idx, c in enumerate(_org_classes):
+            c.__init__ = self._org_inits[idx]
         self.stack.pop(-1)
 
 def within_name_scope(name):
@@ -57,7 +87,6 @@ def within_name_scope(name):
         name (str): Name for setting namespace.
     """
     def decorator(func):
-        import functools
         @functools.wraps(func)
         def wrapper(self, *args, **kwargs):
             with name_scope(name, self.params()):
