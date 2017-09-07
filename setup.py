2b7e68c31575acd3eb3566fa9ce286ee16d7d312
nekanat <nekanat.stock@gmail.com>
2017-09-07 23:48:06 +0900

    add functions

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setup(
     name='tensorboard-chainer',
-    version='0.2.3',
+    version='0.2.4',
     description='Log TensorBoard events with chainer',
     author='nake nat',
     author_email='nakanat.stock@gmail.com',
