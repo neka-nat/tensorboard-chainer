be38ffea20b5dddc84e1d76dc85db77a8b4220db
nekanat <nekanat.stock@gmail.com>
2019-03-08 00:40:45 +0900

    bug fix

--- tb_chainer/name_scope.py
+++ tb_chainer/name_scope.py
@@ -14,8 +14,8 @@ if sys.version_info >= (3, 0):
 else:
     gen_method = lambda m, i, c: MethodType(m, i, c)
 
-def _copy_method(c):
-    g = gen_method(c.__init__, None, c)
+def _copy_method(f, c):
+    g = gen_method(f, None, c)
     return g
 
 def _init_with_name_scope(self, *args, **kargs):
@@ -29,6 +29,13 @@ def _init_with_name_scope(self, *args, **kargs):
     if retain_data and isinstance(self, variable.VariableNode):
         self.retain_data()
 
+def _new_with_name_scope(cls, *args, **kargs):
+    ns = kargs['_name_scope']
+    org_new = kargs['_org_new']
+    self = org_new(cls)
+    self.name_scope = ns
+    return self
+
 _org_classes = []
 _copy_org_inits = []
 
@@ -39,10 +46,13 @@ def register_functions(funcs):
     """
     global _org_classes, _copy_org_inits
     _org_classes.extend(funcs)
-    _copy_org_inits = [_copy_method(c) for c in _org_classes]
+    for c in _org_classes:
+        if c == variable.VariableNode:
+            _copy_org_inits.append(_copy_method(c.__init__, c))
+        else:
+            _copy_org_inits.append(_copy_method(c.__new__, c))
 
-register_functions([m[1] for m in inspect.getmembers(chainer.functions) if inspect.isclass(m[1])] + \
-                   [function_node.FunctionNode, variable.VariableNode])
+register_functions([function_node.FunctionNode, variable.VariableNode])
 
 class name_scope(object):
     """Class that creates hierarchical names for operations and variables.
@@ -69,17 +79,27 @@ class name_scope(object):
 
     def __enter__(self):
         for idx, c in enumerate(_org_classes):
-            self._org_inits.append(c.__init__)
-            c.__init__ = gen_method(functools.partial(_init_with_name_scope,
-                                                      _name_scope='/'.join(self.stack),
-                                                      _org_init=_copy_org_inits[idx],
-                                                      _retain_data=self._retain_data),
-                                    None, c)
+            if c == variable.VariableNode:
+                self._org_inits.append(c.__init__)
+                c.__init__ = gen_method(functools.partial(_init_with_name_scope,
+                                                          _name_scope='/'.join(self.stack),
+                                                          _org_init=_copy_org_inits[idx],
+                                                          _retain_data=self._retain_data),
+                                        None, c)
+            else:
+                self._org_inits.append(c.__new__)
+                c.__new__ = gen_method(functools.partial(_new_with_name_scope,
+                                                         _name_scope='/'.join(self.stack),
+                                                         _org_new=_copy_org_inits[idx]),
+                                        None, c)
         return self
 
     def __exit__(self, exec_type, exec_value, traceback):
         for idx, c in enumerate(_org_classes):
-            c.__init__ = self._org_inits[idx]
+            if c == variable.VariableNode:
+                c.__init__ = self._org_inits[idx]
+            else:
+                c.__new__ = self._org_inits[idx]
         self.stack.pop(-1)
 
 def within_name_scope(name, retain_data=True):
